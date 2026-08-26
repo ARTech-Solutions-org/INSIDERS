9f101f5198a56f1db099d4ca10ddd1f796661cac
Mohanned Mahmoud <m.mahmoud2203@nu.edu.eg>
2026-08-26 13:43:39 +0300

    fix: Bump SW cache and update SHELL_ASSETS for new PWA icons

--- artifacts/ushers-app/public/sw.js
+++ artifacts/ushers-app/public/sw.js
@@ -5,8 +5,8 @@
  */
 
 // 1. PWA App Shell Caching
-const CACHE_NAME = "insiders-shell-v8";
-const SHELL_ASSETS = ["/", "/insiders-logo.png", "/favicon.svg", "/favicon-white.svg", "/manifest.json", "/splash.mp4"];
+const CACHE_NAME = "insiders-shell-v9";
+const SHELL_ASSETS = ["/", "/pwa-icon-192.png", "/pwa-icon-512.png", "/favicon.svg", "/favicon-white.svg", "/manifest.json", "/splash.mp4"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_ASSETS)));
